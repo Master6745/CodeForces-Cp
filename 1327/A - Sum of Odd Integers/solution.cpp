4c131fc93a3b63f84705d12bf8f3359edc0e40ad
Master6745 <ranjannishant024@gmail.com>
2026-09-16 23:02:09 +0530

    Add Sum of Odd Integers [A] from Codeforces

---
+++ 1327/A - Sum of Odd Integers/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define cyes cout<<"YES
+";
+#define cno cout<<"NO
+";
+#define endl "
+"
+#define yesno(check) cout << (check ? "YES" : "NO") << '
+';
+#define all(x) (x).begin(),(x).end()
+#define needforspeed ios::sync_with_stdio(false);cin.tie(nullptr);
+#define vin(v,n) vector<int> v(n); for(auto &x:v) cin>>x;
+#define print(v) for(auto x:v) cout<<x<<" "; cout<<endl;
+signed main(){
+    needforspeed
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        yesno(n>=k*k && n%2==k%2)
+        
+    }
+    return 0;
+ 
+}       
